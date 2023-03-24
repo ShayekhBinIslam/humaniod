1dc81fc698bc892a539041713aa48c4e80696fa0
ShayekhBinIslam <shikh.bin.islam@gmail.com>
2023-03-24 23:14:14 +0600

    fatima with shoot and tl180

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -16,7 +16,7 @@
 Demonstrates the gait manager (inverse kinematics + simple ellipsoid path).
 """
 
-from controller import Robot
+from controller import Robot, Motion
 import sys
 sys.path.append('..')
 # Eve's locate_opponent() is implemented in this module:
@@ -24,6 +24,8 @@ from utils.image_processing import ImageProcessing as IP
 from utils.fall_detection import FallDetection
 from utils.gait_manager import GaitManager
 from utils.camera import Camera
+from utils.current_motion_manager import CurrentMotionManager
+import numpy as np
 
 '''
 Progress:
@@ -34,7 +36,7 @@ Progress:
 
 class Fatima (Robot):
     SMALLEST_TURNING_RADIUS = 0.1
-    # SMALLEST_TURNING_RADIUS = 0.5
+    # SMALLEST_TURNING_RADIUS = 1.0
     SAFE_ZONE = 0.75
     TIME_BEFORE_DIRECTION_CHANGE = 200  # 8000 ms / 40 ms
 
@@ -45,21 +47,56 @@ class Fatima (Robot):
         self.camera = Camera(self)
         self.fall_detector = FallDetection(self.time_step, self)
         self.gait_manager = GaitManager(self, self.time_step)
+        self.current_motion = CurrentMotionManager()
         self.heading_angle = 3.14 / 2
         # Time before changing direction to stop the robot from falling off the ring
         self.counter = 0
+        # load motion files
+        self.motions = {
+            'SideStepLeft': Motion('../motions/SideStepLeftLoop.motion'),
+            'SideStepRight': Motion('../motions/SideStepRightLoop.motion'),
+            'TurnRight': Motion('../motions/TurnRight20.motion'),
+            'TurnLeft': Motion('../motions/TurnLeft20.motion'),
+            'TurnLeft180': Motion('../motions/TurnLeft180.motion'),
+            'TaiChi': Motion('../motions/TaiChi.motion'),
+            'TaiChi_fast': Motion('../motions/TaiChi_fast.motion'),
+            'Shoot': Motion('../motions/Shoot.motion'),
+        }
 
     def run(self):
+        running = 0
         while self.step(self.time_step) != -1:
             # We need to update the internal theta value of the gait manager at every step:
             t = self.getTime()
             self.gait_manager.update_theta()
             if 0.3 < t < 2:
                 self.start_sequence()
+                self.init_motion = self.current_motion.get()
             elif t > 2:
+            # if 1:
                 self.fall_detector.check()
-                self.walk()
 
+                if t < running:
+                    continue
+                else: 
+                    print(t, "Motion end")
+                    running = 0
+                    self.walk()
+                test = np.random.uniform()
+                # if test > 0.99:
+                #     print(t, "TaiChi start")
+                #     self.current_motion.set(self.motions['TaiChi_fast'])
+                #     running = t + 1.3
+                # elif test > 0.98:
+                if test > 0.995:
+                    print(t, "Shoot start")
+                    self.current_motion.set(self.motions['Shoot'])
+                    running = t + 4.8
+                elif test > 0.994:
+                    print(t, "TurnLeft180 start")
+                    self.current_motion.set(self.motions['TurnLeft180'])
+                    running = t + 9.0
+                
     def start_sequence(self):
         """At the beginning of the match, the robot walks forwards to move away from the edges."""
         self.gait_manager.command_to_motors(heading_angle=0)
@@ -71,10 +108,10 @@ class Fatima (Robot):
         # If the opponent is close to the middle, the robot walks straight.
         desired_radius = (self.SMALLEST_TURNING_RADIUS / normalized_x) if abs(normalized_x) > 1e-3 else None
         # TODO: position estimation so that if the robot is close to the edge, it switches dodging direction
-        # if self.counter > self.TIME_BEFORE_DIRECTION_CHANGE:
-        #     self.heading_angle = - self.heading_angle
-        #     self.counter = 0
-        # self.counter += 1
+        if self.counter > self.TIME_BEFORE_DIRECTION_CHANGE:
+            self.heading_angle = - self.heading_angle
+            self.counter = 0
+        self.counter += 1
         
         self.gait_manager.command_to_motors(desired_radius=desired_radius, heading_angle=self.heading_angle)
 
